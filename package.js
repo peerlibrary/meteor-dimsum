fd30ec7b6ab4d870452e4ce1f022c87645012cc4
Mitar <mitar.git@tnode.com>
2014-09-12 22:49:09 -0700

    Migration to Meteor 0.9.0.

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-  summary: "A lorem ipsum generator, generating a filler text for placeholder content"
+  summary: "A lorem ipsum generator, generating a filler text for placeholder content",
+  version: '0.1.8-5',
+  name: 'peerlibrary:dimsum',
+  git: 'https://github.com/peerlibrary/meteor-dimsum.git'
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.1.1');
   api.export('dimsum');
 
   api.add_files([
@@ -12,6 +16,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use(['dimsum', 'tinytest', 'test-helpers'], ['client', 'server']);
+  api.use(['peerlibrary:dimsum', 'tinytest', 'test-helpers'], ['client', 'server']);
+
   api.add_files('tests.js', ['client', 'server']);
 });
--- tests.js
+++ tests.js
@@ -7,7 +7,7 @@ Tinytest.add('dimsum', function (test) {
   catch (e) {
   }
   test.isTrue(isDefined, "dimsum is not defined");
-  test.isTrue(Package.dimsum.dimsum, "Package.dimsum.dimsum is not defined");
+  test.isTrue(Package['peerlibrary:dimsum'].dimsum, "Package.peerlibrary:dimsum.dimsum is not defined");
 
   test.matches(dimsum(), /^[A-Z].+\.$/i);
 });
